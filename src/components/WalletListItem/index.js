ae7170233e2cc2969733f74e54ed404fc32ecf20
1160007652 <1160007652@qq.com>
2020-06-13 08:44:49 +0800

    钱包列表增加随机头像

--- src/components/WalletListItem/index.js
+++ src/components/WalletListItem/index.js
@@ -2,13 +2,14 @@
  * @ Author: Muniz
  * @ Create Time: 2020-06-09 19:27:48
  * @ Modified by: Muniz
- * @ Modified time: 2020-06-12 22:37:30
+ * @ Modified time: 2020-06-13 08:43:14
  * @ Description: 钱包导航, Header组件
  */
 
 import React, { useState } from 'react';
 import { CopyOutlined, CheckOutlined } from '@ant-design/icons';
 import classNames from 'classNames';
+import { NetworkLarge } from 'react-identicon-variety-pack';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './index.less';
@@ -24,7 +25,7 @@ const WalletListItem = ({ data, style, className, onClick }) => {
   }
   return (
     <div className={classNames('wallet-list-item', className)} style={style} onClick={onClick}>
-      <div className="wallet-list-logo"></div>
+      <NetworkLarge className="wallet-list-logo" size={48} seed={data.address} circle />
 
       <div className="wallet-list-text">
         <h3>{data.name}</h3>
