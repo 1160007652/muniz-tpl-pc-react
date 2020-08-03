6e0cb0c0a1a9b9028a08d08fc99c5d7d26c00b28
1160007652 <1160007652@qq.com>
2020-08-03 13:48:29 +0800

    增加重连请求

--- src/dataProxy.js
+++ src/dataProxy.js
@@ -3,7 +3,11 @@
  */
 
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 // import { message } from 'antd';
+
+// 重链请求次数
+axiosRetry(axios, { retries: 3 });
 
 axios.defaults.headers.common.timeout = 10000;
 
