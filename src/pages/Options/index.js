871a37d02d617f34d21216946d0b0b0ebdf89b16
1160007652 <1160007652@qq.com>
2020-11-21 13:39:05 +0800

    增加Readme.md 描述

--- src/pages/Options/index.js
+++ src/pages/Options/index.js
@@ -10,10 +10,7 @@ import './index.less';
 ReactDOM.render(
   <Provider {...rootStore}>
     <div className="about-box">
-      <h3>{intl.get('about_title')}</h3>
-      <div className="version">
-        {intl.get('about_version')}: {process.env.VERSION_APP}
-      </div>
+      <div className="version">{process.env.VERSION_APP}</div>
     </div>
   </Provider>,
   document.getElementById('root'),
